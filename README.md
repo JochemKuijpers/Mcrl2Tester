b66aff4d6c241a78a818866a1c8f5dbf2725efbb
Jochem Kuijpers <github.com@jochemkuijpers.nl>
2017-10-23 14:39:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Mcrl2 Requirement Tester
+
+This project contains scripts and requirements related to our Mcrl2 project.
+
+## Script requirements
+
+Commandline PHP installed
+Mcrl2 tools installed
+
+## Script usage
+
+Use the script as follows
+
+`php modeltest.php [requirements]`
+
+You can use space-separated numbers (`1 2 3`) or ranges (`1-3`), or both, for the requirements.
---
+++ modeltest.php
@@ -0,0 +1,66 @@
+<?php
+
+define("DEF", "\033[1;37m");
+define("RED", "\033[1;31m");
+define("YEL", "\033[1;33m");
+define("BLU", "\033[1;34m");
+define("GRN", "\033[1;32m");
+
+function syscall($cmd) {
+	list($cmdName, $params) = explode(" ", $cmd, 2);
+	echo DEF . "    < " . BLU . $cmd . "\n";
+	$output = [];
+	exec($cmd . " 2>&1", $output, $retval);
+	$lastLine = '';
+	foreach($output as $outLine) {
+		echo DEF . "    > " . YEL . $outLine . "\n";
+		$lastLine = $outLine;
+	}
+	return $lastLine;
+}
+
+function compile() {
+	echo DEF . "Compiling model..\n";
+	syscall("mcrl22lps trio_v2.mcrl2 trio_v2.lps --lin-method=stack");
+	echo GRN . "Compiling complete\n\n";
+}
+
+function testReq($mcfName) {
+	echo DEF . "Testing requirement $mcfName..\n";
+	syscall("lps2pbes -f $mcfName.mcf trio_v2.lps $mcfName.pbes");
+	$lastLine = syscall("pbes2bool $mcfName.pbes");
+	if ($lastLine != "true") {
+		echo RED . "$mcfName invalid\n";
+	} else {
+		echo GRN . "$mcfName valid\n";
+	}
+	echo "\n";
+}
+
+$reqs = [];
+
+if (count($argv) <= 1) {
+	for($n = 1; $n <= 13; $n += 1) {
+		$reqs[] = "systemreq" . $n;
+	}
+}
+
+foreach($argv as $index => $arg) {
+	if (strpos($arg, "-") !== false) {
+		list($from, $to) = explode("-", $arg, 2);
+		for($n = $from; $n <= $to; $n += 1) {
+			$reqs[] = "systemreq" . $n;
+		}
+	} elseif ($index > 0) {
+		$reqs[] = "systemreq" . $arg;
+	}
+}
+
+compile();
+
+foreach($reqs as $req) {
+	testReq($req);
+}
+
+echo DEF . "DONE.\n";
+
