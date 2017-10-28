c7d0cd292b5f622df67687d780c6739e0b024209
Jochem Kuijpers <github.com@jochemkuijpers.nl>
2017-10-28 20:29:24 +0200

    Updated model test script

--- modeltest.php
+++ modeltest.php
@@ -1,31 +1,41 @@
 <?php
 
 define("DEF", "\033[1;37m");
+define("GRY", "\033[0;37m");
 define("RED", "\033[1;31m");
 define("YEL", "\033[1;33m");
 define("BLU", "\033[1;34m");
 define("GRN", "\033[1;32m");
 
+define("SILENT", array_search("--silent", $argv) !== false);
+
 function syscall($cmd) {
-	echo DEF . "    < " . BLU . $cmd . "\n";
+    if (!SILENT) echo DEF . "    < " . BLU . $cmd . "\n";
 	$output = [];
-	exec($cmd . " 2>&1", $output, $retval);
-	$lastLine = '';
-	foreach($output as $outLine) {
-		echo DEF . "    > " . YEL . $outLine . "\n";
-		$lastLine = $outLine;
-	}
-	return $lastLine;
+    echo GRY;
+    list($cmdName, $params) = explode(" ", $cmd, 2);
+    exec("$cmdName $params". (SILENT ? "" : " 2>&1"), $output, $retval);
+    $lastLine = '';
+    foreach($output as $outLine) {
+        if (!SILENT) echo DEF . "    > " . YEL . $outLine . "\n";
+        $lastLine = $outLine;
+    }
+
+    if ($retval !== 0) {
+        echo DEF;
+        die ("$cmdName returned error code $retval\n");
+    }
+    return $lastLine;
 }
 
 function compile() {
-	echo DEF . "Compiling model..\n";
+    if (!SILENT) echo DEF . "Compiling model..\n";
 	syscall("mcrl22lps trio_v2.mcrl2 trio_v2.lps --lin-method=stack");
 	echo GRN . "Compiling complete\n\n";
 }
 
 function testReq($mcfName) {
-	echo DEF . "Testing requirement $mcfName..\n";
+    if (!SILENT) echo DEF . "Testing requirement $mcfName..\n";
 	syscall("lps2pbes -f $mcfName.mcf trio_v2.lps $mcfName.pbes");
 	$lastLine = syscall("pbes2bool $mcfName.pbes");
 	if ($lastLine != "true") {
@@ -33,15 +43,15 @@ function testReq($mcfName) {
 	} else {
 		echo GRN . "$mcfName valid\n";
 	}
-	echo "\n";
+    if (!SILENT) echo "\n";
 }
 
 function showGraph() {
-    echo DEF . "Generating reachable states..\n";
+    if (!SILENT) echo DEF . "Generating reachable states..\n";
     syscall("lps2lts trio_v2.lps trio_v2.aut");
-    echo DEF . "Displaying graph..\n";
+    if (!SILENT) echo DEF . "Displaying graph..\n";
     syscall("ltsgraph trio_v2.aut");
-    echo GRN . "Graph viewer closed\n";
+    if (!SILENT) echo GRN . "Graph viewer closed\n";
 }
 
 $reqs = [];
@@ -61,12 +71,12 @@ if (count($argv) == 2 && $argv[1] == 'graph') {
 }
 
 foreach($argv as $index => $arg) {
-	if (strpos($arg, "-") !== false) {
+	if (preg_match("/^[0-9]+\-[0-9]+$/", $arg)) {
 		list($from, $to) = explode("-", $arg, 2);
 		for($n = $from; $n <= $to; $n += 1) {
 			$reqs[] = "systemreq" . $n;
 		}
-	} elseif ($index > 0) {
+	} elseif ($index > 0 && is_numeric($arg)) {
 		$reqs[] = "systemreq" . $arg;
 	}
 }
